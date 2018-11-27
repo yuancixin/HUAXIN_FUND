bcecc8a5d4cd39ee82d1c1ebbb8ab5cb0d0e28dc
yuancixin <462156387@qq.com>
2018-11-27 10:38:23 +0800

    画图

--- python_test/src/trading_strategy/strategy1.py
+++ python_test/src/trading_strategy/strategy1.py
@@ -10,6 +10,7 @@ import csv
 from config.properties import SOURCE_DIR
 import matplotlib.pyplot as plt
 
+
 def strategy(code1, code2):
 
     df1 = df[df['ts_code'] == code1][['trade_date', 'close', 'vol', 'oi']].sort_values('trade_date', ascending=True)
@@ -22,16 +23,38 @@ def strategy(code1, code2):
     result[['close1', 'close2', 'vol1', 'vol2', 'oi1', 'oi2']] = result[['close1', 'close2', 'vol1', 'vol2', 'oi1', 'oi2']].apply(pd.to_numeric)
     
     result['difference'] = result['close1'] - result['close2']
-    result['ave_difference5'] = np.round(pd.Series.rolling(result['difference'],window = 5).mean(),2)
-    
+#     result['ave_difference'] = np.round(pd.Series.rolling(result['difference'], window=20).mean(), 2)
     return result 
 
+
+def get_ave_difference(result, day):
+    return np.round(pd.Series.rolling(result['difference'], window=day).mean(), 2)
+
+def show(code1,code2,day):
+    result = strategy(code1, code2)
+    result['ave_difference%s' % day] = get_ave_difference(result, day)
+    fig = plt.figure(1)
+    plt.subplot(311)
+    plt.plot(result['trade_date'], result['difference'], c='red', label='difference')
+    plt.plot(result['trade_date'], result['ave_difference%s' % day], c='blue', label='ave_difference%s' % day)
+    plt.legend(loc='upper left')
+    plt.subplot(312)
+    plt.plot(result['trade_date'], result['oi1'], c='red', label='oi1')
+    plt.plot(result['trade_date'], result['oi2'], c='blue', label='oi2')
+    plt.legend(loc='upper left')
+    plt.subplot(313)
+    plt.plot(result['trade_date'], result['vol1'], c='red', label='vol1')
+    plt.plot(result['trade_date'], result['vol2'], c='blue', label='vol2')
+    plt.legend(loc='upper left')
+    
+    plt.show()
 
 if __name__ == '__main__':
     
     df = pd.read_csv('%s/future_data.csv' % SOURCE_DIR, quoting=csv.QUOTE_NONE)
     code1 = 'C 01'
     code2 = 'C 05'
-    result = strategy(code1, code2)
-    print(result)
+    day = 60
+    show(code1,code2,day)
+#     print(result)
     
