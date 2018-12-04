93e1da7a7649ead7e0ec07cdc6516d69e1765244
yuancixin <462156387@qq.com>
2018-12-04 11:01:08 +0800

    修改回测输出

--- python_test/src/backtest/backtest1.py
+++ python_test/src/backtest/backtest1.py
@@ -24,29 +24,39 @@ def backtest1(df, code, N):
         nonlocal B_PRICE, hold_num
         B_PRICE = price
         hold_num = capital_available / B_PRICE
-        print('以%s买入' % price)
+#         print('以%s买入' % price)
 
     def sell(price):
         nonlocal S_PRICE, hold_num
         S_PRICE = price
         hold_num = capital_available / S_PRICE
-        print('以%s卖出' % price)
+#         print('以%s卖出' % price)
         
     def unwind(S, B):
-        nonlocal capital_available, hold_num, B_PRICE, S_PRICE
+        nonlocal capital_available, hold_num, B_PRICE, S_PRICE, trade_num , capital_last, retreat_max
         capital_available = hold_num * (S - B) * 10 + capital_available
-        print('平仓，当前总资产%s，S--%s,B--%s' % (capital_available, S, B))
+        
+        retreat = (capital_available - capital_last) / capital_last * 100
+        
+        if retreat < retreat_max:
+            retreat_max = retreat
+        capital_last = capital_available
+#         print('平仓，当前总资产%s，S--%s,B--%s' % (capital_available, S, B))
         hold_num = 0
         B_PRICE = 0
         S_PRICE = 0
+        trade_num = trade_num + 1
     
     capital_base = 1000000  # 初始资本
     capital_available = 1000000  # 可用资金
+    capital_last = 1000000  # 上次资产
+    retreat_max = 0  # 最大回撤率
     
-    hold_num = 0
-    trade_trend = '平'
     B_PRICE = 0
     S_PRICE = 0
+    trade_trend = '平'
+    hold_num = 0
+    trade_num = 0
     
     df_code = df[df['ts_code'] == code][['trade_date', 'close', 'year']].sort_values('trade_date', ascending=True)
     df_code['ave_close%s' % N] = get_ave_line(df_code['close'], N)
@@ -58,56 +68,49 @@ def backtest1(df, code, N):
         i = 1
         while(i < df_year.shape[0] - 1):
             if df_year[i - 1:i]['difference'].values[0] * df_year[i:i + 1]['difference'].values[0] <= 0:
-                print('======%s触发交易======' % df_year[i:i + 1]['trade_date'].values[0])
+#                 print('======%s触发交易======' % df_year[i:i + 1]['trade_date'].values[0])
 #                 击穿
                 if trade_trend == '平':
                     if df_year[i:i + 1]['difference'].values[0] > df_year[i - 1:i]['difference'].values[0]:  # 多
                         buy(df_year[i:i + 1]['close'].values[0])
-#                         print('%s以%s买入' % (df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
                         trade_trend = '多'
                     elif df_year[i:i + 1]['difference'].values[0] < df_year[i - 1:i]['difference'].values[0]:  # 空
                         sell(df_year[i:i + 1]['close'].values[0])
-#                         print('%s以%s卖出' % (df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
                         trade_trend = '空'
                 
                 elif trade_trend == '多':
                     unwind(df_year[i:i + 1]['close'].values[0] , B_PRICE)
-#                     print('%s以%s平仓' %(df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
                     trade_trend = '空'
                     sell(df_year[i:i + 1]['close'].values[0])
-#                     print('%s以%s卖出' % (df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
                     
                 elif trade_trend == '空':
                     unwind(S_PRICE , df_year[i:i + 1]['close'].values[0])
-#                     print('%s以%s平仓' %(df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
                     trade_trend = '多'
-                    buy(df_year[i:i + 1]['close'].values[0])
-#                     print('%s以%s买入' % (df_year[i:i + 1]['trade_date'].values[0], df_year[i:i + 1]['close'].values[0]))
-                    
+                    buy(df_year[i:i + 1]['close'].values[0])              
             else:
                 pass
                         
             i = i + 1 
         
         if trade_trend == '多':
-            print('======%s因合约结束，平仓======' % df_year[i:i + 1]['trade_date'].values[0])
+#             print('======%s因合约结束，平仓======' % df_year[i:i + 1]['trade_date'].values[0])
             unwind(df_year[i:i + 1]['close'].values[0] , B_PRICE)
             trade_trend = '平'
         elif trade_trend == '空':
-            print('======%s因合约结束，平仓======' % df_year[i:i + 1]['trade_date'].values[0])
+#             print('======%s因合约结束，平仓======' % df_year[i:i + 1]['trade_date'].values[0])
             unwind(S_PRICE , df_year[i:i + 1]['close'].values[0])
             trade_trend = '平'
         else:
             pass
-            
-    print('最终资产%s' % capital_available)
-
-#         print(df_year)
+    
+    print('%s日均线--交易次数%s--单次最大回撤%s--最终资产%s' % (N, trade_num, round(retreat_max, 2), round(capital_available, 2)))
 
 
 if __name__ == '__main__':
     df_source = pd.read_csv('%s/future_data.csv' % SOURCE_DIR, quoting=csv.QUOTE_NONE)
     
-    N = 60
     code = 'JD 05'
-    backtest1(df_source, code, N)
+    
+    for N in range(10, 121, 5):
+        backtest1(df_source, code, N)
+    
