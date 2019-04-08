8e7d0021dea8f716b1b16f33fa510adb4dc866ff
yuancixin <462156387@qq.com>
2019-04-08 14:56:37 +0800

    1

--- python_test/src/trading_strategy/stock_strategy_NM.py
+++ python_test/src/trading_strategy/stock_strategy_NM.py
@@ -23,9 +23,10 @@ def get_code_near_ave(df, N, M):
     获取N日均线附近的股票
     """
     result = []
+    print("读取数据...")
     df_baseinfo = pd.read_csv('%s/stock_baseinfo.csv' % SOURCE_DIR, quoting=csv.QUOTE_NONE)
     all_codes = df_baseinfo['ts_code']
-    
+    print("读取数据完成")
     for code in all_codes:
         df1 = df[df['ts_code'] == code][['trade_date', 'close']].sort_values('trade_date', ascending=True)
         if df1.shape[0] > 0:
@@ -33,8 +34,8 @@ def get_code_near_ave(df, N, M):
             df1['close'] = df1['close'].apply(pd.to_numeric)
             if df1['ave_close%s' % N].iat[-1] * (1 - M) < df1['close'].iat[-1] < df1['ave_close%s' % N].iat[-1] * (1 + M) :
                 result.append(code)
-
-    print('%s日均线附近的股票：' % N)
+    
+    print('%s日均线附近 %s%% 的合约：' % (N, M * 100))
     print(result)
     return result
 
@@ -46,6 +47,6 @@ if __name__ == '__main__':
     end_date = 20181231
     df_source = df_source[df_source['trade_date'] > strat_date][['ts_code','trade_date','close']]
     
-    N = 11
+    N = 120
     M = 0.01
     get_code_near_ave(df_source, N, M)
